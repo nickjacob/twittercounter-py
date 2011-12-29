844e5d036ba46d8c23b2010e851633bb643530be
Nick Jacob <nicholas_jacob@brown.edu>
2011-12-29 18:12:35 -0500

    changed from tweepy to python-twitter, removed any authentication for twitter API, automatically check if it's a screenname so you can pass either.

--- twittercounter.py
+++ twittercounter.py
@@ -8,36 +8,27 @@
 from urllib2 import HTTPError,Request,urlopen,URLError
 from urllib import urlencode
 from simplejson import load
-from tweepy import OAuthHandler, API
+import twitter
 
 class API:
 	"""
 	arguments:
 		api_key - twittercounter.com API key
-		twitter - dict for twitter.com API credentials. should have
-		'consumer_key' and 'consumer_secret'
 	usage:
 		instantiate API, call it 
 	"""
-	def __init__(self,api_key,twitter={}):
-		""" sets up initial values. twitter should be a dict if to be used"""
-		self.twitter = twitter
+	def __init__(self,api_key):
 		self.api_key = api_key 
 		self.TC_URL = 'http://api.twittercounter.com/'
 		self.calls = {} # storage for calls
 	
-	def set_twitter_credentials(consumer_key,consumer_secret):
-		""" mutator for twitter credentials so you don't have to instantiate with them
-		"""
-		self.twitter['consumer_key'] = consumer_key
-		self.twitter['consumer_secret'] = consumer_secret
 	def gen_id(self,screen_name):
 		""" returns the user id from twitter, to simplify calls to api
 		"""
-		auth = OAuthHandler(self.twitter['consumer_key'],self.twitter['consumer_secret'])
-		api = API(auth)
+		if isinstance(screen_name,int): return scree_name
+		api = twitter.Api()
 		try:
-			name = api.get_user(screen_name).id
+			name = api.GetUser(screen_name).id
 		except:
 			name = 0
 		return name
@@ -48,11 +39,7 @@ class API:
 			doesn't make a new call if it's for the same screenname to reduce
 			API calls.
 		"""
-		if self.twitter: 
-			u_id = self.gen_id(user)
-		else: 
-			u_id = user
-		
+		u_id = self.gen_id(user) # convert if necessary
 		# minimize API calls - check if you already looked
 		if str(u_id) in self.calls: 
 			self.data = self.calls[str(u_id)]
